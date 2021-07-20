c13792e96dd3cc1773982dad13459c43abc7188a
sravanthi487 <laxmiprasannapanangipalli123@gmail.com>
2021-07-20 15:17:49 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,12 @@
+
+<!-- Connection Page -->
+
+<?php
+
+	$conn = mysqli_connect('localhost','id17275600_prasanna','u\*@z7</FpBh(EoR','id17275600_db_mybank');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
